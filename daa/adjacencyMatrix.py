726a6d668a6c4f8f12cb1a8bbb820df04c0f7895
Mohammed Haris <141272429+haris410@users.noreply.github.com>
2024-02-04 04:06:51 +0530

    Update adjacencyMatrix.py

--- daa/adjacencyMatrix.py
+++ daa/adjacencyMatrix.py
@@ -3,14 +3,14 @@
 num_vertices = int(input("Enter the number of vertices: "))
 num_edges = int(input("Enter the number of edges: "))
 
-adjacency_matrix = [[0] * num_vertices for _ in range(num_vertices)]
+adj_matrix = [[0] * num_vertices for _ in range(num_vertices)]
 
 print("Enter edges (vertex1 vertex2):")
 for _ in range(num_edges):
-    vertex1, vertex2 = map(int, input().split())
-    adjacency_matrix[vertex1][vertex2] = 1
-    adjacency_matrix[vertex2][vertex1] = 1  # For undirected graph
+    v1, v2 = map(int, input().split())
+    adj_matrix[v1][v2] = 1
+    adj_matrix[v2][v1] = 1  # For undirected graph
 
 print("\nAdjacency Matrix:")
-for row in adjacency_matrix:
+for row in adj_matrix:
     print(row)
